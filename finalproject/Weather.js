6c66243e90559ebc0571bb29b3edf0c3b8171e4e
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2023-11-06 17:47:42 -0500

    12321

--- finalproject/Weather.js
+++ finalproject/Weather.js
@@ -8,43 +8,67 @@ import {
   View,
   ScrollView,
   Dimensions,
+  ActivityIndicator,
 } from "react-native";
 import { theme } from "./color";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 const { width: SCREEN_SIZE } = Dimensions.get("window");
 
-export const WeatherScreen = ({ navigation }) => {
-  const [click, setClick] = useState(false);
+const API_KEYS = "ecf8fcdc109cf9fd31f4211a5edc67e8";
 
+export const WeatherScreen = ({ navigation }) => {
+  const [city, setCity] = useState("Loading...");
+  const [click, setClick] = useState(false);
+  const [days, setDays] = useState([]);
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEYS}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={{ flex: 6, backgroundColor: "#74b9ff" }}>
         <View style={styles.city}>
-          <Text style={styles.cityName}>Bloomington</Text>
+          <Text style={styles.cityName}>{city}</Text>
         </View>
         <ScrollView
           horizontal
           pagingEnabled
+          showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.weather}
         >
-          <View style={styles.day}>
-            <Text style={styles.temp}>3</Text>
-            <Text style={styles.weatherLook}>Sunny</Text>
-          </View>
-          <View style={styles.day}>
-            <Text style={styles.temp}>3</Text>
-            <Text style={styles.weatherLook}>Sunny</Text>
-          </View>
-          <View style={styles.day}>
-            <Text style={styles.temp}>3</Text>
-            <Text style={styles.weatherLook}>Sunny</Text>
-          </View>
-          <View style={styles.day}>
-            <Text style={styles.temp}>3</Text>
-            <Text style={styles.weatherLook}>Sunny</Text>
-          </View>
+          {days.length === 0 ? (
+            <View style={styles.day}>
+              <ActivityIndicator />
+            </View>
+          ) : (
+            <View style={styles.day}></View>
+          )}
         </ScrollView>
       </View>
       <View style={{ flex: 1, backgroundColor: "#5B6265" }}>
